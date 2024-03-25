668d122f5f68076ef609bb3de4ac8515328b5705
Brandon Lingo <brandonlingo0@gmail.com>
2024-03-25 03:48:12 -0700

    add bishop check functions

--- src/headers/bishopMoves.hpp
+++ src/headers/bishopMoves.hpp
@@ -1,11 +1,76 @@
 #include <iostream>
 
+bool upperLeftCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		std::cout << i << std::endl;
+		std::cout << board[pieceRow - i][pieceCol - i] << std::endl;
+
+		// Checks if a piece is in the path of the moveSpot
+		if (board[pieceRow - i][pieceCol - i] != "-") {
+			std::cout << "bishop UL check function";
+			std::cout << board[pieceRow - i][pieceCol - i] << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+		if ((pieceRow - i - 1 == moveRow) && (pieceCol - i - 1 == moveCol)) {
+			return true;
+			break;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool upperRightCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		std::cout << i << std::endl;
+		std::cout << board[pieceRow - i][pieceCol + i] << std::endl;
+		std::cout << board[pieceRow - i ][pieceCol + i ]   << std::endl;
+
+		if (board[pieceRow - i][pieceCol + i] != "-") {
+			std::cout << "bishop UR check function";
+			std::cout << board[pieceRow - i][pieceCol + i] << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+
+		// Checks if a piece is in the path of the moveSpot
+		if ((pieceRow - i - 1 == moveRow) && (pieceCol + i + 1 == moveCol)) {
+			return true;
+			break;
+		}
+
+		i++;
+	}
+	return true;
+}
+
+bool lowerLeftCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	return true;
+}
+
+bool lowerRightCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	return true;
+}
+
 bool whiteBishopMove(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
 
 	std::string moveSpot = board[moveRow][moveCol];
 	int bishopRow = abs(moveRow - pieceRow);
 	int bishopCol = abs(moveCol - pieceCol);
+	bool validCheck;
 
+	// Player is not allowed to move a spot that has a black piece or enemy king
 	if (moveSpot[0] != 'W' && moveSpot[1] != 'K') {
 
 		//std::cout << "Valid knight move 1" << std::endl;
@@ -15,10 +80,18 @@ bool whiteBishopMove(int moveRow, int moveCol, int pieceRow, int pieceCol, std::
 
 			//std::cout << "Valid knight move 2" << std:: endl;
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
+			if ((moveRow - pieceRow <= -1) && (moveCol - pieceCol <= -1)) {
+				validCheck = upperLeftCheck(moveRow, moveCol, pieceRow, pieceCol, board);
+			}
+			else if ((moveRow - pieceRow <= -1) && (moveCol - pieceCol >= 1)) {
+				validCheck = upperRightCheck(moveRow, moveCol, pieceRow, pieceCol, board);
+			}
+			if (validCheck) {
+				board[moveRow][moveCol] = board[pieceRow][pieceCol];
+				board[pieceRow][pieceCol] = "-";
 
-			return true;
+				return true;
+			}
 		}
 		else {
 			std::cout << "Not a valid Bishop move!" << std::endl;
@@ -35,11 +108,12 @@ bool whiteBishopMove(int moveRow, int moveCol, int pieceRow, int pieceCol, std::
 
 bool blackBishopMove(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
 
-	std::string friendlyPiece = board[moveRow][moveCol];
+	std::string moveSpot = board[moveRow][moveCol];
 	int bishopRow = abs(moveRow - pieceRow);
 	int bishopCol = abs(moveCol - pieceCol);
 
-	if (friendlyPiece[0] != 'B') {
+	// Player is not allowed to move a spot that has a black piece or enemy king
+	if (moveSpot[0] != 'B' && moveSpot[1] != 'K') {
 
 		//std::cout << "Valid knight move 1" << std::endl;
 
