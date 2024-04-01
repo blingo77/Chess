98bfc174df2112331d883a2fa17b078a1c71fbd7
Brandon Lingo <brandonlingo0@gmail.com>
2024-04-01 11:03:59 -0700

    add: roock checking functions

--- src/headers/chess.hpp
+++ src/headers/chess.hpp
@@ -161,7 +161,6 @@ public:
 
 			drawBoard();
 			blackMove();
-
 		}
 	}
 
--- src/headers/rookMoves.hpp
+++ src/headers/rookMoves.hpp
@@ -1,10 +1,91 @@
 #include <iostream>
 
+bool leftHorizontalCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		if ((pieceRow == moveRow) && (pieceCol + i == moveCol)) {
+			return true;
+			break;
+		}
+		else if (board[pieceRow][pieceCol + i] != "-") {
+			std::cout << "Rook Lh check function" << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool rightHorizontalCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		if ((pieceRow == moveRow) && (pieceCol - i == moveCol)) {
+			return true;
+			break;
+		}
+		else if (board[pieceRow][pieceCol - i] != "-") {
+			std::cout << "Rook Rh check function" << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool verticalUpCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		if ((pieceRow - i == moveRow) && (pieceCol == moveCol)) {
+			return true;
+			break;
+		}
+		else if (board[pieceRow - i][pieceCol] != "-") {
+			std::cout << "Rook VU check function" << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool verticalDownCheck(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+
+	int i = 1;
+	while (true) {
+
+		if ((pieceRow + i == moveRow) && (pieceCol == moveCol)) {
+			return true;
+			break;
+		}
+		else if (board[pieceRow + i][pieceCol] != "-") {
+			std::cout << "Rook Lh check function" << std::endl;
+			std::cout << "There is a piece in your way!" << std::endl;
+			return false;
+			break;
+		}
+		i++;
+	}
+	return true;
+}
+
 bool whiteRookMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
 
 	std::string moveSpot = board[moveRow][moveCol];
 	int rookRow = pieceRow - moveRow;
 	int rookCol = pieceCol - moveCol;
+	bool validCheck;
 
 	if (moveSpot[0] != 'W' && moveSpot[1] != 'K') {
 
@@ -13,29 +94,24 @@ bool whiteRookMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::s
 		std::cout << pieceCol - moveCol << std::endl;
 		if (rookRow == 0 && rookCol < 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = leftHorizontalCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 		else if (rookRow == 0 && rookCol > 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = rightHorizontalCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 
 		// Check for vertical movement
 		if (rookRow > 0 && rookCol == 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = verticalUpCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 		else if (rookRow < 0 && rookCol == 0) {
 
+			validCheck = verticalDownCheck(moveRow, moveCol, pieceRow, pieceCol, board);
+		}
+
+		if (validCheck) {
 			board[moveRow][moveCol] = board[pieceRow][pieceCol];
 			board[pieceRow][pieceCol] = "-";
 
@@ -56,6 +132,7 @@ bool blackRookMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::s
 	std::string moveSpot = board[moveRow][moveCol];
 	int rookRow = pieceRow - moveRow;
 	int rookCol = pieceCol - moveCol;
+	bool validCheck;
 
 	if (moveSpot[0] != 'B' && moveSpot[1] != 'K') {
 
@@ -64,29 +141,24 @@ bool blackRookMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::s
 		std::cout << pieceCol - moveCol << std::endl;
 		if (rookRow == 0 && rookCol < 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = leftHorizontalCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 		else if (rookRow == 0 && rookCol > 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = rightHorizontalCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 
 		// Check for vertical movement
 		if (rookRow > 0 && rookCol == 0) {
 
-			board[moveRow][moveCol] = board[pieceRow][pieceCol];
-			board[pieceRow][pieceCol] = "-";
-
-			return true;
+			validCheck = verticalUpCheck(moveRow, moveCol, pieceRow, pieceCol, board);
 		}
 		else if (rookRow < 0 && rookCol == 0) {
 
+			validCheck = verticalDownCheck(moveRow, moveCol, pieceRow, pieceCol, board);
+		}
+
+		if (validCheck) {
 			board[moveRow][moveCol] = board[pieceRow][pieceCol];
 			board[pieceRow][pieceCol] = "-";
 
