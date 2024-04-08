d50a68b9b7c9a855f6b80a05f53c72eae3907937
Brandon Lingo <brandonlingo0@gmail.com>
2024-04-07 17:32:27 -0700

    add queen code

---
+++ src/headers/queenMoves.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+bool whiteQueenMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+	return true;
+}
+
+
+bool blackQueenMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::string board[][9]) {
+	return true;
+}
