0aab7e7c365c4019c95916e70ab55b504b9054d6
Brandon Lingo <brandonlingo0@gmail.com>
2024-04-14 19:33:56 -0700

    add: white king movement

--- src/headers/kingMoves.hpp
+++ src/headers/kingMoves.hpp
@@ -4,11 +4,27 @@ bool whiteKingMoves(int moveRow, int moveCol, int pieceRow, int pieceCol, std::s
 
 	std::string moveSpot = board[moveRow][moveCol];
 
+	int kingRow = abs(moveRow - pieceRow);
+	int kingCol = abs(moveCol - pieceCol);
+
+	// Checks if the spot is friendly or king piece
 	if (moveSpot[0] != 'W' && moveSpot[1] != 'K') {
+		// Checks if the king has a valid move
+		if ((kingRow == 1 || kingRow == 0) && (kingCol == 1 || kingCol == 0)) {
+
+			board[moveRow][moveCol] = board[pieceRow][pieceCol];
+			board[pieceRow][pieceCol] = "-";
+
+			return true;
+		}
+		else {
+			std::cout << "That is not a valid King move!" << std::endl;
+			return false;
+		}
 
 	}
 	else {
-		std::cout << "That is a friendly piece!";
+		std::cout << "That is a friendly piece!" << std::endl;
 		return false;
 	}
 
