38802099c9c82b98bef984c8897a6774e9857dad
Brandon Lingo <brandonlingo0@gmail.com>
2024-04-23 14:24:15 -0700

    add: bishop leftLowerKingCheck

--- src/check headers/bishopPreCheck.hpp
+++ src/check headers/bishopPreCheck.hpp
@@ -1,26 +1,70 @@
 #include <iostream>
 #include <vector>
 
+bool lowerLeftKingCheck(int bishopRow, int bishopCol, int kingRow, int kingCol, std::string board[][9]) {
+
+	// FIND A WAY TO CHECK BOTH BLACK AND WHITE KING
+
+	int i = 1;
+	while (true) {
+
+		// Catches the bug of reading memeory address out of bounds
+		if (i < 0 || i > 9) {
+			break;
+		}
+
+		if (board[bishopRow + i][bishopCol - i] == "WK") {
+			std::cout << "You are in check! MOVE" << std::endl;
+			return true;
+			break;
+		}
+		i++;
+	}
+
+	return false;
+}
 
 bool whiteBishopPreCheck(int kingRow, int kingCol, std::string board[][9]) {
 
-	std::vector<int> bishopRows;
-	std::vector<int> bishopCol;
+	int bishopRow;
+	int bishopCol;
 
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			if (board[i][j] == "Bb") {
-				bishopRows.push_back(i);
-				bishopCol.push_back(j);
+				bishopRow = i;
+				bishopCol = j;
+
+				if (lowerLeftKingCheck(bishopRow, bishopCol, kingRow, kingCol, board)) {
+
+					return true;
+				}
 			}
 		}
 	}
 
-	return true;
+	return false;
 
 }
 
 bool blackBishopPreCheck(int kingRow, int kingCol, std::string board[][9]) {
 
-	return true;
+	int bishopRow;
+	int bishopCol;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (board[i][j] == "Bb") {
+				bishopRow = i;
+				bishopCol = j;
+
+				if (lowerLeftKingCheck(bishopRow, bishopCol, kingRow, kingCol, board)) {
+
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
 }
